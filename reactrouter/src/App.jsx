cee91ce1feac28c2c932b937f616a55dfa48cf7e
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-04-14 09:14:26 -0400

    first commit

---
+++ reactrouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
