4262bd22b4f4b2e4cbdae12b4f7be27dc0026e1f
bruceathy <67832593+bruceathy@users.noreply.github.com>
2025-04-16 09:10:38 -0400

    useN avigaate

--- reactrouter/src/pages/HomePage.jsx
+++ reactrouter/src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h1>Home Page</h1>
       <Link to="/products">Products</Link>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
+      </p>
     </>
   );
 }
